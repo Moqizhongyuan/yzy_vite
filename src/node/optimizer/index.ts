45f2ebd46b8872a7799945770ba1857c5577eeec
Moqizhongyuan <2356373295@qq.com>
2024-11-22 00:46:00 +0800

    ✨新增(javascript): 依赖预构建
    基于esbuild打包

---
+++ src/node/optimizer/index.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+import { build } from 'esbuild'
+import { green } from 'picocolors'
+import { scanPlugin } from './scanPlugin'
+import { PRE_BUNDLE_DIR } from '../constants'
+export async function optimize(root: string) {
+  // 确定构建入口
+  const entry = path.resolve(root, 'src/main.tsx')
+
+  // 扫描依赖
+  const deps = new Set<string>()
+  await build({
+    entryPoints: [entry],
+    bundle: true,
+    write: false,
+    plugins: [scanPlugin(deps)]
+  })
+
+  await build({
+    entryPoints: [...deps],
+    write: true,
+    bundle: true,
+    format: 'esm',
+    splitting: true,
+    outdir: path.resolve(root, PRE_BUNDLE_DIR),
+    plugins: []
+  })
+
+  console.log(
+    `${green('需要预构建的依赖')}:\n${[...deps]
+      .map(green)
+      .map(item => `  ${item}`)
+      .join('\n')}`
+  )
+}
